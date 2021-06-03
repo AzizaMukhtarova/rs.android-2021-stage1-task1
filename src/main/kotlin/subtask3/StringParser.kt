95e77964a440b5aa46f1085d911c2318435d0eed
AzizaMukhtarova <AzizaMukhtarova@users.noreply.github.com>
2021-06-03 20:27:10 +0500

    done with stringParser

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -2,8 +2,27 @@ package subtask3
 
 class StringParser {
 
-    // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val array = mutableListOf<String>()
+        var current = 0
+        for (i in inputString.withIndex()) {
+            if (i.value == '[' ||i.value == '<' ||  i.value == '(') {
+                val end = when (i.value) {
+                    '<' -> '>'
+                    '[' -> ']'
+                    '(' -> ')'
+                    else -> ' '
+                }
+                for (j in i.index + 1 until inputString.length) {
+                    if (inputString[j] == i.value) current++
+                    if (inputString[j] == end) {
+                        if (current == 0) {
+                            array.add(inputString.substring(i.index + 1 until j))
+                        } else current--
+                    }
+                }
+            }
+        }
+        return array.toTypedArray()
     }
 }
